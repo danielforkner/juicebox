5154d2f64a755eba2d504c9172a1b22d65802295
danielforkner <daniel.forkner@gmail.com>
2022-03-25 13:45:47 -0400

    adding jwt tokens

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
-const PORT = 3000;
-const express = require("express");
-const server = express();
-const apiRouter = require("./api");
-const morgan = require("morgan");
+require('dotenv').config();
 
-server.use(morgan("dev"));
+// remove this once you confirm it works
+console.log(process.env.JWT_SECRET);
+
+const PORT = 3000;
+const express = require('express');
+const server = express();
+const apiRouter = require('./api');
+const morgan = require('morgan');
+
+server.use(morgan('dev'));
 server.use(express.json());
 
 server.use((req, res, next) => {
-  console.log("<____Body Logger START____>");
+  console.log('<____Body Logger START____>');
   console.log(req.body);
-  console.log("<_____Body Logger END_____>");
+  console.log('<_____Body Logger END_____>');
 
   next();
 });
 
-server.use("/api", apiRouter);
+server.use('/api', apiRouter);
 
-const { client } = require("./db");
+const { client } = require('./db');
 client.connect();
 
 server.listen(PORT, () => {
-  console.log("Server is live on port:", PORT);
+  console.log('Server is live on port:', PORT);
 });
