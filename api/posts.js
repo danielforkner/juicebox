897818626409d4ff48379f69808507492da98e47
Derrick Hinh <derhin1@gmail.com>
2022-03-28 09:48:11 -0500

    Finished post /api/posts

--- api/posts.js
+++ api/posts.js
@@ -1,11 +1,46 @@
 const express = require("express");
 const postsRouter = express.Router();
-const { getAllPosts } = require("../db");
+const { getAllPosts, createPost, getUserByUsername } = require("../db");
+const { requireUser } = require("./utils");
 
 postsRouter.get("/", async (req, res, next) => {
   const posts = await getAllPosts();
 
   res.send({ posts });
+});
+
+postsRouter.post("/", requireUser, async (req, res, next) => {
+  const { title, content, tags = "" } = req.body;
+
+  const tagArr = tags.trim().split(/\s+/);
+  const postData = {};
+
+  if (tagArr.length) {
+    postData.tags = tagArr;
+  }
+
+  try {
+    const { id } = await getUserByUsername(req.user.username);
+    const authID = id;
+    postData.authorID = authID;
+    postData.title = title;
+    postData.content = content;
+    console.log(postData);
+    const post = await createPost(postData);
+    if (post) {
+      res.send({ post });
+    } else {
+      next({
+        name: "Create Post Error",
+        message: "Failed to create post",
+      });
+    }
+  } catch ({ name, message }) {
+    next({
+      name,
+      message,
+    });
+  }
 });
 
 module.exports = postsRouter;
