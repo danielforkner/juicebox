6d02b348e6690da60fe8ce48f5495f818a34026d
danielforkner <daniel.forkner@gmail.com>
2022-03-28 13:22:55 -0400

    fixed error

--- api/index.js
+++ api/index.js
@@ -45,10 +45,6 @@ apiRouter.use((req, res, next) => {
   next();
 });
 
-apiRouter.use('/', (req, res, next) => {
-  req.message('you are on the api page');
-  next();
-});
 apiRouter.use('/users', usersRouter);
 apiRouter.use('/posts', postsRouter);
 apiRouter.use('/tags', tagsRouter);
