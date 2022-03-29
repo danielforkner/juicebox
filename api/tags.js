d3c794c50ac2d6f1dd89e13d1606e30ed4684a5c
danielforkner <daniel.forkner@gmail.com>
2022-03-29 14:23:59 -0400

    added get route for /tags

--- api/tags.js
+++ api/tags.js
@@ -2,11 +2,11 @@ const express = require('express');
 const tagsRouter = express.Router();
 const { getAllTags, getPostsByTagName } = require('../db');
 
-// tagsRouter.use('/', async (req, res) => {
-//   const tags = await getAllTags();
+tagsRouter.use('/', async (req, res) => {
+  const tags = await getAllTags();
 
-//   res.send({ tags });
-// });
+  res.send({ tags });
+});
 
 tagsRouter.get('/:tagName/posts', async (req, res, next) => {
   const { tagName } = req.params;
