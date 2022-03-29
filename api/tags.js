64b9abcdfca02279a31d9b94698b3b51f054eaf9
danielforkner <daniel.forkner@gmail.com>
2022-03-29 14:24:07 -0400

    fix typo

--- api/tags.js
+++ api/tags.js
@@ -2,7 +2,7 @@ const express = require('express');
 const tagsRouter = express.Router();
 const { getAllTags, getPostsByTagName } = require('../db');
 
-tagsRouter.use('/', async (req, res) => {
+tagsRouter.get('/', async (req, res) => {
   const tags = await getAllTags();
 
   res.send({ tags });
