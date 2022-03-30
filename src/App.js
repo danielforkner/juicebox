61380fabc9a3e6aed92606119516f2a1db9252cb
danielforkner <daniel.forkner@gmail.com>
2022-03-30 14:59:07 -0400

    added some styling

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import React, { useState } from "react";
-import { Route } from "react-router-dom";
-import axios from "axios";
+import React, { useState } from 'react';
+import { Route } from 'react-router-dom';
+import axios from 'axios';
 
 function App() {
   const [posts, setPosts] = useState([]);
   const [tags, setTags] = useState([]);
   async function clickHandler() {
-    const response = await axios.get("/api/posts");
+    const response = await axios.get('/api/posts');
     setPosts(response.data.posts);
   }
   async function clickHandler2() {
-    const response = await axios.get("/api/tags");
-    console.log(response.data.tags, "test");
+    const response = await axios.get('/api/tags');
+    console.log(response.data.tags, 'test');
     setTags(response.data.tags);
   }
   return (
@@ -19,7 +19,17 @@ function App() {
       <button onClick={clickHandler}>Get Posts</button>
       <div className="posts">
         {posts.map((post) => {
-          return <div>{post.title}</div>;
+          return (
+            <div className="postCard">
+              <h2>{post.title}</h2>
+              <p>{post.content}</p>
+              <p>
+                {post.tags.map((tag) => {
+                  return `${tag} `;
+                })}
+              </p>
+            </div>
+          );
         })}
       </div>
 
